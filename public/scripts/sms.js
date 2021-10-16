1f19c19b3a3e7c6c25cd292d566b3c2a02e690af
habibcodes <habib.codes@gmail.com>
2021-10-16 19:50:46 -0400

    set up Twilio env, outbound routes, and connect active number for in/outgoing sms

--- public/scripts/sms.js
+++ public/scripts/sms.js
@@ -1,4 +1,4 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
+
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -7,8 +7,8 @@ const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+15017122661',
-    to: '+15558675310'
+    body: 'AlphaBetaDeltaGamma',
+    from: '+6474961279', // our number
+    to: '+6476462240'
   })
   .then(message => console.log(message.sid));
