1784012ab8bbaede8e46ae7269f5ddb700883328
habibcodes <habib.codes@gmail.com>
2021-10-19 23:06:44 -0400

    minor db correction for chicken img link

--- db/seeds/05_food_items.sql
+++ db/seeds/05_food_items.sql
@@ -5,7 +5,7 @@ INSERT INTO food_items
  (2, 1, 'Montreal Smoked Meat Sandwich', 'https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg', 'main', 'beef brisket, coriander, black pepper, bay leaf', 13.99),
  (3, 1, 'Coke', 'https://www.mcdonalds.com/content/dam/usa/nfl/nutrition/items/regular/desktop/t-mcdonalds-Coca-Cola-Classic-Small.jpg', 'beverage', 'sugar, carbonated water, colouring', 2.99),
  (4, 1, 'Boxty Breakfast', 'https://www.themealdb.com/images/media/meals/naqyel1608588563.jpg', 'breakfast', 'potatoes, bacon, eggs', 10.99),
- (5, 1, 'Chicken Karaage', 'https://www.youtube.com/watch?v=XivddFddthc', 'main', 'chicken thigh, ginger, garlic soy sauce', 19.99),
+ (5, 1, 'Chicken Karaage', 'https://static01.nyt.com/images/2018/07/25/dining/HK-karaage-horizontal/merlin_141075300_74569dec-9fc2-4174-931d-019dddef3bb8-articleLarge.jpg', 'main', 'chicken thigh, ginger, garlic soy sauce', 19.99),
  (6, 1, 'Beef and Oyster pie', 'https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg', 'main', 'beef, oysters, shallots, stout', 21.99),
  (7, 1, 'Chicken Quinoa Greek Salad', 'https://www.themealdb.com/images/media/meals/k29viq1585565980.jpg', 'salad', 'quinoa, chicken breast, black olives, red onions', 7.99),
  (8, 1, 'Dundee Cake', 'https://www.themealdb.com/images/media/meals/wxyvqq1511723401.jpg', 'dessert', 'almonds, orange, appricot jam, glace cherry', 8.99),
