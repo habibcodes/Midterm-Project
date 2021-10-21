e43e76b2efd7bf1c27aa793c3289c9dbf0c97236
habibcodes <habib.codes@gmail.com>
2021-10-21 16:30:29 -0400

    fix img files styling; seed db menu_itms with new menu

--- db/seeds/05_food_items.sql
+++ db/seeds/05_food_items.sql
@@ -1,14 +1,14 @@
 INSERT INTO food_items
 (order_id, restaurant_id, name, img, type, ingredient, price)
  VALUES
- (1, 1, 'Beef Wellington', 'https://www.themealdb.com/images/media/meals/vvpprx1487325699.jpg', 'main', 'beef, mushrooms, puff pastry, mustard', 10),
- (2, 1, 'Smoked Meat Sandwich', 'https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg', 'main', 'beef brisket, coriander, black pepper, bay leaf', 13.99),
+ (1, 1, 'Steak & Fries', 'https://images.unsplash.com/photo-1600891964092-4316c288032e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80', 'main', 'fillet mignon, steak rub, fries, salt', 10),
+ (2, 1, 'Gourmet Burger', 'https://images.unsplash.com/photo-1586816001966-79b736744398?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80', 'main', 'beef brisket, coriander, black pepper, bay leaf', 13.99),
  (3, 1, 'Coke', 'https://www.mcdonalds.com/content/dam/usa/nfl/nutrition/items/regular/desktop/t-mcdonalds-Coca-Cola-Classic-Small.jpg', 'beverage', 'sugar, carbonated water, colouring', 2.99),
- (4, 1, 'Boxty Breakfast', 'https://www.themealdb.com/images/media/meals/naqyel1608588563.jpg', 'breakfast', 'potatoes, bacon, eggs', 10.99),
- (5, 1, 'Chicken Karaage', 'https://static01.nyt.com/images/2018/07/25/dining/HK-karaage-horizontal/merlin_141075300_74569dec-9fc2-4174-931d-019dddef3bb8-articleLarge.jpg', 'main', 'chicken thigh, ginger, garlic soy sauce', 19.99),
- (6, 1, 'Beef and Oyster pie', 'https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg', 'main', 'beef, oysters, shallots, stout', 21.99),
- (7, 1, 'Chicken Quinoa Salad', 'https://www.themealdb.com/images/media/meals/k29viq1585565980.jpg', 'salad', 'quinoa, chicken breast, black olives, red onions', 7.99),
- (8, 1, 'Dundee Cake', 'https://www.themealdb.com/images/media/meals/wxyvqq1511723401.jpg', 'dessert', 'almonds, orange, appricot jam, glace cherry', 8.99),
- (9, 1, 'Summer Pudding', 'https://www.themealdb.com/images/media/meals/rsqwus1511640214.jpg', 'dessert', 'strawberries, blackberries, redcurrants, raspberries', 4.99),
+ (4, 1, 'Pristine Pancake', 'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg', 'breakfast', 'maple syrub, butter, flour', 10.99),
+ (5, 1, 'Artisanal Pizza', 'https://www.freegreatpicture.com/files/104/15234-hd-pizza-gourmet.jpg', 'main', 'olives, mozzarella, tomato sauce', 19.99),
+ (6, 1, 'Speghetti and Meatballs', 'https://images.unsplash.com/photo-1589227365533-cee630bd59bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80', 'main', 'beef, olive oil, shallots, tomato sauce', 21.99),
+ (7, 1, 'Chicken Quinoa Salad', 'https://images.unsplash.com/photo-1580013759032-c96505e24c1f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1209&q=80', 'salad', 'quinoa, chicken breast, black olives, red onions', 7.99),
+ (8, 1, 'Dundee Cake', 'https://images.unsplash.com/photo-1517427294546-5aa121f68e8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80', 'dessert', 'chocolate, flour, chocolate cream, glace cherry', 8.99),
+ (9, 1, 'Chocolate Pudding', 'https://images.unsplash.com/photo-1620406348453-a6966a6c1833?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80', 'dessert', 'vanilla cream, chocolate cream, sugar, butter', 4.99),
  (10, 1, 'Mineral Water', 'https://fitcasually.com/wp-content/uploads/2019/03/evian_pack24x330mL_new_bottle_c_img60.jpg', 'beverage', 'water', 4)
 ;
